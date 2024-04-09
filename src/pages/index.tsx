000a21fc820d51052a1fa0c5b5bfed2b577f9171
Key5n <90585006+Key5n@users.noreply.github.com>
2024-04-09 00:23:36 +0000

    swr with Suspense の waterfall と conditional requests の問題の検証（ただし失敗）

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,7 @@ async function delay(time: number) {
 }
 
 async function fetcher(url: string) {
+  console.log(url);
   const response = await fetch(url);
   await delay(1000);
 
@@ -17,18 +18,60 @@ async function fetcher(url: string) {
 const dummy = { id: 1, title: "ダミー" };
 
 function Component() {
-  const { data } = useSWR(
-    "https://api.github.com/repos/vercel/swr/issues",
+  const { data: swrIssues } = useSWR(
+    "https://api.github.com/repos/vercel/swr/issues?per_page=3",
     fetcher,
-    { suspense: true, fallbackData: [dummy] }
+    { suspense: true, fallbackData: [dummy] },
+  );
+  const { data: nextjsIssues } = useSWR(
+    "https://api.github.com/repos/vercel/next.js/issues?per_page=3",
+    fetcher,
+    { suspense: true, fallbackData: [dummy] },
+  );
+  const { data: swrIssueTopUser } = useSWR(
+    () => swrIssues[0].user.url,
+    fetcher,
+    {
+      suspense: true,
+      fallbackData: [dummy],
+    },
+  );
+  const { data: nextjsIssueTopUser } = useSWR(
+    () => nextjsIssues[0].user.url,
+    fetcher,
+    {
+      suspense: true,
+      fallbackData: [dummy],
+    },
   );
 
   return (
-    <ul>
-      {data.map((v: any) => {
-        return <li key={v.id}>{v.title}</li>;
-      })}
-    </ul>
+    <div>
+      <ul>
+        {swrIssues.map((v: any) => {
+          return (
+            <li key={v.id} style={{ color: "red" }}>
+              {v.title}
+            </li>
+          );
+        })}
+      </ul>
+      <ul>
+        {nextjsIssues.map((v: any) => {
+          return (
+            <li key={v.id} style={{ color: "blue" }}>
+              {v.title}
+            </li>
+          );
+        })}
+      </ul>
+      <div style={{ color: "red" }}>
+        Top user name in swr: {swrIssueTopUser.name}
+      </div>
+      <div style={{ color: "blue" }}>
+        Top user name in Next.js: {nextjsIssueTopUser.name}
+      </div>
+    </div>
   );
 }
 
